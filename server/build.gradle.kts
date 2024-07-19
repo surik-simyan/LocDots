7ce51d3626e654dacab0bae22d5c5d1f2794f4bd
surik-simyan <surik.simyan@gmail.com>
2024-07-20 00:26:28 +0400

    removed http

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "surik.simyan.locdots"
-version = "1.0.2"
+version = "1.0.4"
 application {
     mainClass.set("surik.simyan.locdots.ApplicationKt")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
